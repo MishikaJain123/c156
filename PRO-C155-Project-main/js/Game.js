c4ede9eeae83be6bb243eab1eed15f78ca9a96af
MishikaJain123 <155298829+MishikaJain123@users.noreply.github.com>
2024-02-18 17:02:30 +0530

    Add files via upload

---
+++ PRO-C155-Project-main/js/Game.js
@@ -0,0 +1,60 @@
+AFRAME.registerComponent("game-play", {
+    schema: {
+      elementId: { type: "string", default: "#coin1" },      
+    },
+    init:function(){
+var duration=120;
+var timer=document.querySelector("#timer")
+this.startTimer(duration,timer)
+    },
+    updateScore: function(){
+      var element = document.querySelector("#score");
+      var count = element.getAttribute("text").value;
+var currentScore = parseInt(count);
+currentScore += 50;
+element.setAttribute("text",{
+  value: currentScore,
+});
+    },
+    startTimer:function(duration,timerEl){
+      var minutes;
+      var seconds;
+      setInterval(()=> {
+        if (duration>=0){
+          minutes = parseInt(duration / 60);
+          seconds = parseInt(duration % 60);
+               
+                if(minutes<10){
+                  minutes = "0" + minutes;
+                }
+                if(seconds < 10){
+                  seconds = "0" +seconds;
+                }
+                timerEl.setAttribute("text",{
+                  value:minutes = ":" + seconds,
+                });
+                duration -= 1;
+              }
+      },1000
+      )
+    },
+    update: function() {
+      this.isCollided(this.data.elementId);
+    },
+  
+    isCollided: function(elementId) {
+      const element = document.querySelector(elementId);
+      element.addEventListener("collide", e => {
+        if (elementId.includes("#coin")) {          
+          console.log("coin collision");
+          element.setAttribute("visible",false)
+          this.updateScore();
+        }
+        else if(elementId.includes("#fish")){
+          console.log("fish collision");
+        }         
+      });
+    }
+    
+  });
+  
